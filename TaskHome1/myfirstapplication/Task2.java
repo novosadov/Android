67eac4001b2b90afbf4941f16a479ce67a627d62
Pavel Novosadov <berk@mail.ru>
2017-07-25 18:12:16 +0300

    Create Task2.java

---
+++ TaskHome1/myfirstapplication/Task2.java
@@ -0,0 +1,13 @@
+package com.pnovosdovgmail.myfirstapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Task2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task2);
+    }
+}
