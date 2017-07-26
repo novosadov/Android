eb120c2c42b700d057100478a4cc7ba64b13f267
Pavel Novosadov <berk@mail.ru>
2017-07-26 20:55:08 +0300

    Create MainActivity.java

---
+++ 26072017/MainActivity.java
@@ -0,0 +1,19 @@
+package com.pnovosdovgmail.test;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends Activity {
+
+    Button buttonDz1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
